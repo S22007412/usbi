88fd54cc897b12a346b5b50e4a86826de494ef51
ZS22007412 <ZS22007412@gmail.com>
2026-03-19 06:26:25 -0600

    Eliminar archivos PHP redundantes

--- biblioteca/dbtest.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = new PDO("mysql:host=ubiuv.duckdns.org;port=3306;dbname=usbi", "usbi", "contraseña");
-    echo "Database connection successful!";
-} catch(Exception $e) {
-    echo "Error: " . $e->getMessage();
-}
-?>
--- biblioteca/debug.php
+++
@@ -1,8 +0,0 @@
-<?php
-ini_set("display_errors", 1);
-error_reporting(E_ALL);
-require_once "config/database.php";
-$db = new Database();
-$connection = $db->getConnection();
-echo "Database connection works!";
-?>
